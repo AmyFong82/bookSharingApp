a45e1f67ec1cfc05d2af967aab3dbaf705f29585
AmyFong82 <amyhfong@gmail.com>
2021-07-13 12:01:59 -0400

    starting on the frontend

--- book-sharing-backend/app/controllers/users_controller.rb
+++ book-sharing-backend/app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
 	def show
-		user = User.find()
+		user = User.find(params[:id])
+		render json: user
 
 	end
 
--- book-sharing-backend/config/routes.rb
+++ book-sharing-backend/config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :reviews
   resources :requests
   resources :books
-  resources :users
+  resources :users, only: [:show, :destroy] do 
+    resources :books
+    
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+#index, show, create, update, destroy
