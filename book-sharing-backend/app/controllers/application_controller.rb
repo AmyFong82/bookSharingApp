7596eb7a3cdb46a3a3b7a77f379210c881fa8033
AmyFong82 <amyhfong@gmail.com>
2021-07-14 10:12:02 -0400

    added .env file to store the secret for the JWT token.

--- book-sharing-backend/app/controllers/application_controller.rb
+++ book-sharing-backend/app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+	before_action :authorized
+
+
+	def encode_token(payload) #{ user_id: 1 }
+		JWT.encode(payload, 'my_s3cr3t')
+	end
+
+	def auth_header
+		 # { 'Authorization': 'Bearer <token>' }
+		request.headers['Authorization'] #Bearer <token>
+	end
+
+	def decode_token
+		if auth_header()
+			token = auth_header.split(' ')[1] #[Bearer, <token>]
+			begin
+				JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+			rescue JWT::DecodeError
+				nil
+			end
+		end	
+	end
 end
