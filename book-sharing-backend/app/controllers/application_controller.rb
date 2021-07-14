ea18cd6f91a15532490cedc2f5ab9bc3bde9087f
AmyFong82 <amyhfong@gmail.com>
2021-07-14 13:08:04 -0400

    set jwt for authorization.

--- book-sharing-backend/app/controllers/application_controller.rb
+++ book-sharing-backend/app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::API
     end
 
 	def encode_token(payload) #{ user_id: 1 }
-		# JWT.encode(payload, 'my_s3cr3t')
 		JWT.encode(payload, jwt_key)
 	end
 
@@ -20,9 +19,27 @@ class ApplicationController < ActionController::API
 			token = auth_header.split(' ')[1] #[Bearer, <token>]
 			begin
 				JWT.decode(token, jwt_key, true, algorithm: 'HS256')
+		        # JWT.decode => [{ "user_id"=>"1" }, { "alg"=>"HS256" }]
 			rescue JWT::DecodeError
 				nil
 			end
 		end	
+	end
+
+	def current_user
+		if decoded_token()
+			user_id = decoded_token[0]['user_id'] #[{ "user_id"=>"1" }, { "alg"=>"HS256" }]
+			@user = User.find_by(id: user_id)
+		else
+			nil
+		end
+	end
+
+	def logged_in?
+		!!current_user
+	end
+
+	def authorized
+		render json: { message: 'Please log in'}, status: :unauthorized unless logged_in?
 	end
 end
