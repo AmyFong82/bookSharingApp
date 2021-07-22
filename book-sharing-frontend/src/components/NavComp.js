f54681e7597739fbb28cf6a2b1d46367d1fa0c5c
AmyFong82 <amyhfong@gmail.com>
2021-07-22 12:50:46 -0400

    changed the site title color to blue

--- book-sharing-frontend/src/components/NavComp.js
+++ book-sharing-frontend/src/components/NavComp.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 
 class NavComp extends Component {
 	render(){
 		return (
-			<Navbar bg="light" expand="lg">
+			<Navbar bg="light" expand="lg" fixed="top">
 			  <Container>
-			  <Navbar.Brand href="#home">Welcome to Book Sharing</Navbar.Brand>
+			  <Navbar.Brand href="#home"><i class="fas fa-book"></i> <b>Book Sharing</b></Navbar.Brand>
 			    <Navbar.Toggle aria-controls="basic-navbar-nav" />
 			    <Navbar.Collapse id="basic-navbar-nav">
 			      <Nav className="me-auto">
-			        <Nav.Link href="#home">Home</Nav.Link>
-			        <Nav.Link href="#link">Link</Nav.Link>
+			        <Nav.Link href="/books">Books</Nav.Link>
+			        <Nav.Link href="/account">Account</Nav.Link>
 	              </Nav>
+			      <Button variant="outline-primary" >Login / Sign Up</Button>
 			    </Navbar.Collapse>
 			  </Container>
 			</Navbar>
