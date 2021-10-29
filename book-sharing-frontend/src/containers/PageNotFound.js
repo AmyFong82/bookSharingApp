dfe13ba6b54566510bec62cbf48213b3a525ad7a
AmyFong82 <amyhfong@gmail.com>
2021-10-29 10:33:12 -0400

    added PageNotFound component.

---
+++ book-sharing-frontend/src/containers/PageNotFound.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
+
+export default class PageNotFound extends Component {
+	render(){
+		return(
+			<div className="account-container mt-5">
+				<h1> The page you are trying to visit does not exist.</h1>
+				<Link to={`/books/`} className="btn btn-primary">Home</Link>
+			</div>
+		)
+	}
+}
