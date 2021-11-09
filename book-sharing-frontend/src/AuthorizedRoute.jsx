0837783b919f2ba03a0eda93ae850ad386586475
AmyFong82 <amyhfong@gmail.com>
2021-11-09 08:58:59 -0500

    made the AuthorizedRoute.jsx, will test it out.

--- book-sharing-frontend/src/AuthorizedRoute.jsx
+++ book-sharing-frontend/src/AuthorizedRoute.jsx
@@ -1,7 +1,25 @@
+import { Route } from 'react-router-dom'
 import { loggedIn } from './components/users/loggedIn'
+import { redirectToLogin } from './containers/redirectToLogin'
 
 export function AuthorizedRoute({children, ...rest}) {
 	const auth = loggedIn(this.props.loginStatus)
+
+	return (
+		<Route 
+			{...rest}
+			render={({location}) => 
+				auth ? (
+					children
+				) : (
+					redirectToLogin()
+				)
+
+			}
+
+		/>
+
+	)
 
 	
 }
